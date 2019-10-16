4047c58edbeaa4df8d7bee6e5832dacf312e4c0a
Alessandro Mattei <alessandro.mattei1@student.univaq.it>
2019-10-16 12:15:38 +0200

    debug

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,8 +3,8 @@ import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
   { path: '', redirectTo: 'home', pathMatch: 'full' },
-  { path: 'home', loadChildren: './home/home.module#HomePageModule' },
-  { path: 'chat', loadChildren: './chat/chat.module#ChatPageModule' },
+  { path: 'home', loadChildren: './pages/home/home.module#HomePageModule' },
+  { path: 'chat', loadChildren: './pages/chat/chat.module#ChatPageModule' },
 ];
 
 @NgModule({
