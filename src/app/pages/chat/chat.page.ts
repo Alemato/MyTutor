500e0fc2bf07c17532aed54b67e07d51d2353113
Stefano Decina <stefano.decina@student.univaq.it>
2019-10-16 17:24:59 +0200

    prova storage su home per kevin

--- src/app/pages/chat/chat.page.ts
+++ src/app/pages/chat/chat.page.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {TranslateService} from '@ngx-translate/core';
 import {NavController} from '@ionic/angular';
 import { Storage } from '@ionic/storage';
@@ -9,14 +9,20 @@ import { Storage } from '@ionic/storage';
   styleUrls: ['./chat.page.scss'],
 })
 export class ChatPage implements OnInit {
-
+  public risposta: string;
+  public messaggio: string;
+  public cont: string;
+  public contProv: string;
   constructor(
       private translateService: TranslateService,
       private navController: NavController,
       private storage: Storage
   ) { }
-
   ngOnInit() {
+    // abbozzo di storage con promise fatta male
+    this.storage.get('messaggio1').then((val) => {
+      this.risposta = val;
+    });
     this.initTranslate();
   }
   initTranslate() {
--- src/app/pages/home/home.page.ts
+++ src/app/pages/home/home.page.ts
@@ -1,12 +1,23 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import { Storage } from '@ionic/storage';
 
 @Component({
-  selector: 'app-home',
-  templateUrl: 'home.page.html',
-  styleUrls: ['home.page.scss'],
+    selector: 'app-home',
+    templateUrl: 'home.page.html',
+    styleUrls: ['home.page.scss'],
 })
-export class HomePage {
+export class HomePage implements OnInit {
+    public messaggio: string;
+    public risposta: string;
 
-  constructor() {}
-
+    constructor(
+        private storage: Storage
+    ) {
+    }
+    public mex() {
+        this.risposta = 'ricevuto ' + this.messaggio;
+        this.storage.set('messaggio' , this.risposta);
+    }
+    ngOnInit() {
+    }
 }
