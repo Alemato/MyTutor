afad4f6f627f31813504ebd21d6f3396ea10c2d6
Titi Kevin <titikevin93@gmail.com>
2019-10-17 17:02:59 +0200

    get e set di chat

--- src/app/services/chat.service.ts
+++ src/app/services/chat.service.ts
@@ -1,4 +1,8 @@
 import { Injectable } from '@angular/core';
+import {Storage} from '@ionic/storage';
+import {Observable} from 'rxjs';
+import {fromPromise} from 'rxjs/internal-compatibility';
+import {LINGUA} from '../constants';
 
 export class ChatMessage {
   messageId: string;
@@ -24,7 +28,43 @@ export const userAvatar = 'https://gravatar.com/avatar/dba6bae8c566f9d4041fb9cd9
 })
 
 export class ChatService {
-
-  constructor() { }
+  messaggio: ChatMessage;
+  constructor(private storage: Storage) { }
   // inserire metodi per prendere i messaggi dallo storage;
+  // getMessageId(): Observable<string> {
+  // return fromPromise(this.storage.get());
+  // }
+  getMessageId(): string {
+    return this.messaggio.messageId;
+  }
+  setMessageId(messageeeId: string) {
+    this.messaggio.messageId = messageeeId;
+  }
+  getUserId(): string {
+    return this.messaggio.userId;
+  }
+  getUserName(): string {
+    return this.messaggio.userName;
+  }
+  getUserAvatar(): string {
+   return this.messaggio.userAvatar;
+  }
+  getToUserId(): string {
+    return this.messaggio.toUserId;
+  }
+  getTime(): number | string {
+    return this.messaggio.time;
+  }
+  getMessage(): string {
+    return this.messaggio.message;
+  }
+  getStatus(): string {
+    return this.messaggio.status;
+  }
+  getMessaggio(): ChatMessage {
+    return this.messaggio;
+  }
+  // getMessaggio(): Observable<ChatMessage> {
+  //   return fromPromise(this.storage.get('ll').get)
+  // }
 }
