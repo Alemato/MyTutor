f1e394b91512755070b71a6f51d60889d3e6fea9
Titi Kevin <titikevin93@gmail.com>
2019-10-17 15:37:47 +0200

    input messaggio chat

--- src/app/pages/chat/chat.page.ts
+++ src/app/pages/chat/chat.page.ts
@@ -13,11 +13,19 @@ export class ChatPage implements OnInit {
   public messaggio: string;
   public cont: string;
   public contProv: string;
+  public messaggio1: string;
+  public messaggio2: string;
   constructor(
       private translateService: TranslateService,
       private navController: NavController,
       private storage: Storage
   ) { }
+  public  mex1() {
+    console.log(this.messaggio1);
+    this.messaggio2 = this.messaggio1;
+    this.messaggio1="";
+    this.storage.set('messaggio2', 'this.messaggio2');
+  }
   ngOnInit() {
     // abbozzo di storage con promise fatta male
     this.storage.get('messaggio1').then((val) => {
