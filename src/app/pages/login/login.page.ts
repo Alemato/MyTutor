b1ced6605ecfdef322155b08ea5df6614adc2c4f
Alessandro Mattei <alessandro.mattei1@student.univaq.it>
2019-10-22 16:03:56 +0200

    Bozza Login

---
+++ src/app/pages/login/login.page.ts
@@ -0,0 +1,29 @@
+import {Component, OnInit} from '@angular/core';
+
+@Component({
+    selector: 'page-login',
+    templateUrl: './login.page.html',
+    styleUrls: ['./login.page.scss'],
+})
+export class LoginPage implements OnInit {
+
+    passwordType: string = 'password';
+    passwordShow: boolean = false;
+
+    constructor() {
+    }
+
+    public togglePassword() {
+        if (this.passwordShow) {
+            this.passwordShow = false;
+            this.passwordType = 'password';
+        } else {
+            this.passwordShow = true;
+            this.passwordType = 'text';
+        }
+    }
+
+    ngOnInit() {
+    }
+
+}
