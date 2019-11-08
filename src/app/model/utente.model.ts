43b0a91e9cc05ba478e88e7f8a27b11d115de08f
Titi Kevin <titikevin93@gmail.com>
2019-11-08 10:26:06 +0100

    utente model

---
+++ src/app/model/utente.model.ts
@@ -0,0 +1,14 @@
+export class Utente {
+    nome: string;
+    cognome: string;
+    email: string;
+    password: string;
+    datanascita: string;
+    regione: string;
+    citta: string;
+    cap: number;
+    via: string;
+    civico: number;
+    biografia: string;
+    docstud: boolean;
+}
