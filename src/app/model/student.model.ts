bca8b882d020894b5d7068abf1a36462ccf359cb
Stefano Decina <stefano.decina@student.univaq.it>
2019-11-12 17:12:09 +0100

    costruttore model user

--- src/app/model/student.model.ts
+++ src/app/model/student.model.ts
@@ -3,4 +3,8 @@ import {User} from './user.model';
 export  class Student {
     user: User;
     studyGrade: string;
+    constructor(obj: any) {
+        this.user = new User(obj.user);
+        this.studyGrade = obj.studyGrade;
+    }
 }
