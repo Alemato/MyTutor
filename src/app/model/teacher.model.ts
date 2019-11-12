aed73f6a01aeaf18806b09315db3cc4902d33c19
Stefano Decina <stefano.decina@student.univaq.it>
2019-11-12 17:17:46 +0100

    costruttore model teacher

--- src/app/model/teacher.model.ts
+++ src/app/model/teacher.model.ts
@@ -8,4 +8,14 @@ export class Teacher {
     street: string;
     streetNumber: string;
     byography: string;
+
+    constructor(obj: any) {
+        this.user = new User(obj.user);
+        this.postCode = obj.postCode;
+        this.city = obj.city;
+        this.region = obj.region;
+        this.street = obj.street;
+        this.streetNumber = obj.streetNumber;
+        this.byography = obj.byography;
+    }
 }
