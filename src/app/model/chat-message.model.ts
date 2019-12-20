87c7eabca91be0a521c1b9cbaae2890ee975fe90
Stefano Decina <stefano.decina@student.univaq.it>
2019-12-20 09:46:55 +0100

    Bozza Model Chat Message

---
+++ src/app/model/chat-message.model.ts
@@ -0,0 +1,10 @@
+export class ChatMessage {
+    messageId: number;
+    userId: number;
+    userName: string;
+    userAvatar: string;
+    toUserId: number;
+    time: number | string;
+    message: string;
+    status: string;
+}
