7649109f6c4dc638c6a8723e1f3b04966322e2f5
Alessandro Mattei <alessandro.mattei1@student.univaq.it>
2020-01-06 16:34:22 +0100

    BOZZA Primissima Lezione

---
+++ src/app/pages/lezione/lezione.page.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-lezione',
+  templateUrl: './lezione.page.html',
+  styleUrls: ['./lezione.page.scss'],
+})
+export class LezionePage implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
