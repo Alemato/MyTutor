53c119c7f1844f3d6c85213c903daa287467c7d8
Alessandro Mattei <alessandro.mattei1@student.univaq.it>
2020-01-10 12:27:43 +0100

    modifica URL

--- src/app/constants.ts
+++ src/app/constants.ts
@@ -3,25 +3,23 @@ export const USE_PROXY = true;
 
 // export const URL_BASE = USE_PROXY ? 'api' : 'http://192.168.0.201:8080/api';     // SERVER NOSTRO
 export const URL_BASE = USE_PROXY ? 'api' : 'http://localhost:8080/api';
-export const URL_BASE2 = USE_PROXY ? 'todos' : 'http://jsonplaceholder.typicode.com/todos';    // SERVER DI PROVA
-export const URL_BASE3 = USE_PROXY ? 'posts' : 'https://jsonplaceholder.typicode.com';
 
 export const URL = {
-    LOGIN: URL_BASE + '/authentication-1',
+    LOGIN: URL_BASE + '/auth',
     GET_PROFILO: URL_BASE + '/auth/profiles',
+    PUT_PROFILO_STUDENT: URL_BASE + '/auth/profiles/student',
+    PUT_PROFILO_TEACHER: URL_BASE + '/auth/profiles/teacher',
     REGISTRATION_STUDENT: URL_BASE + '/registration/student',
     REGISTRATION_TEACHER: URL_BASE + '/registration/teacher',
     TEST: URL_BASE + '/test/aut',
     CHATMESSAGE: URL_BASE + '/chatmessage',
     CHAT_POST: URL_BASE + '/chats/post',
-    TESTTODOS: URL_BASE2 + '/1',
     MESSAGE: URL_BASE + '/chats/1/messaggi',
-    REGISTRAZIONE: URL_BASE3,
-
     HOME_BOOKING: URL_BASE + 'bookings-lessons/home',
 };
 
 export const X_AUTH = 'X-Auth';
+export const AUTH = 'authorization';
 
 export const AUTH_TOKEN = 'auth-token';
 export const LINGUA = 'lingua';
