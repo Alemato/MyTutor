a5d26852a1c668ab6a71c228c8b782279a564359
Stefano Decina <stefano.decina@student.univaq.it>
2020-01-15 11:18:22 +0100

    bozza constants

--- src/app/constants.ts
+++ src/app/constants.ts
@@ -15,8 +15,36 @@ export const URL = {
     CHATMESSAGE: URL_BASE + '/chatmessage',
     CHAT_POST: URL_BASE + '/chats/post',
     MESSAGE: URL_BASE + '/chats/1/messaggi',
-    HOME_BOOKING: URL_BASE + 'bookings-lessons/home',
-    PLANNING: URL_BASE + '/lessons/plannings',
+
+
+    CHATLIST: URL_BASE + '/chats',
+    CHAT_CREATE: URL_BASE + '/chats/create',
+    CHAT_COUNT: URL_BASE + '/chats/create/count',
+
+    MESSAGE_OF_CHAT_PT_1: URL_BASE + '/chat/',
+    MESSAGE_OF_CHAT_PT_2: '/message',
+    MESSAGE_COUNT: '/count',
+    LAST_MESSAGES: '/countmessage',
+
+    SUBJECT: URL_BASE + '/subject',
+
+    LESSON: URL_BASE + '/lessons',
+    LESSON_MODIFY: URL_BASE + '/lessons/modify',
+
+    PLANNING_CREATE: URL_BASE + '/lessons/plannings/create',
+    PLANNING_MODIFY: URL_BASE + '/lessons/plannings/modify',
+    PLANNING_ADD: URL_BASE + '/lessons/plannings/add',
+    PLANNING_RESEARCH: URL_BASE + '/lessons/plannings/research',
+    PLANNING_DELETE: URL_BASE + '/lessons/plannings/delete',
+
+    BOOKING: URL_BASE + '/lessons/bookings',
+    BOOKING_MODIFY_LESSON_STATE: URL_BASE + '/lessons/bookings',
+
+    BOOKING_RESEARCH: URL_BASE + '/bookings-lessons/research',
+    BOOKING_HOME: URL_BASE + '/bookings-lessons/home',
+    BOOKING_HISTORY: URL_BASE + '/bookings-lessons/history',
+    BOOKING_HISTORY_FILTER: URL_BASE + '/bookings-lessons/history/filter',
+    BOOKING_COUNT: URL_BASE + '/bookings-lessons/count'
 };
 
 export const X_AUTH = 'X-Auth';
@@ -29,6 +57,11 @@ export const UTENTE_STORAGE = 'utente';
 export const MARIO = 'pluto';
 
 export const STORAGE = {
-    BOOKING : 'booking',
-    HISTORY : 'storico',
+    BOOKING: 'booking',
+    HISTORY: 'storico',
+    MESSAGE: 'message',
+    CHATLIST: 'chat',
+    CREATES: 'creates',
+    PLANNING: 'planning'
+
 };
