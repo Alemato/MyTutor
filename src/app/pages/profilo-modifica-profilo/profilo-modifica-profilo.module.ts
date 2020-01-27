f5e1dcf75f9394864c36ba1d70ac8dedba5775e7
Titi Kevin <titikevin93@gmail.com>
2020-01-27 16:29:48 +0100

    Finale Profilo-Modifica-Profilo tradotto

--- src/app/pages/profilo-modifica-profilo/profilo-modifica-profilo.module.ts
+++ src/app/pages/profilo-modifica-profilo/profilo-modifica-profilo.module.ts
@@ -7,6 +7,7 @@ import { IonicModule } from '@ionic/angular';
 
 import { ProfiloModificaProfiloPage } from './profilo-modifica-profilo.page';
 import {SuperTabsModule} from '@ionic-super-tabs/angular';
+import {TranslateModule} from '@ngx-translate/core';
 
 const routes: Routes = [
   {
@@ -21,7 +22,8 @@ const routes: Routes = [
         FormsModule,
         IonicModule,
         RouterModule.forChild(routes),
-        SuperTabsModule
+        SuperTabsModule,
+        TranslateModule
     ],
   declarations: [ProfiloModificaProfiloPage]
 })
