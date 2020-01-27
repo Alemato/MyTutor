bdc2ed450809a6cf80b3ff75c13e71ba4ca78e07
Alessandro Mattei <alessandro.mattei1@student.univaq.it>
2020-01-27 10:58:26 +0100

    Debug create service

--- src/app/services/create.service.ts
+++ src/app/services/create.service.ts
@@ -1,10 +1,8 @@
 import {Injectable} from '@angular/core';
-import {Storage} from '@ionic/storage';
 import {HttpClient, HttpResponse} from '@angular/common/http';
 import {CreatesChat} from '../model/creates.model';
 import {Observable} from 'rxjs';
-import {fromPromise} from 'rxjs/internal-compatibility';
-import {STORAGE, URL} from '../constants';
+import {URL} from '../constants';
 import {map} from 'rxjs/operators';
 
 @Injectable({
@@ -13,7 +11,6 @@ import {map} from 'rxjs/operators';
 export class CreateService {
 
     constructor(
-        private storage: Storage,
         private http: HttpClient
     ) {
     }
@@ -31,41 +28,5 @@ export class CreateService {
             .pipe(map((resp: HttpResponse<any>) => {
             return resp;
         }));
-    }
-
-    getStorageCreateList(): Observable<CreatesChat[]> {
-        return fromPromise(this.storage.get(STORAGE.CREATES));
-    }
-
-    setStorageCreates(creates: CreatesChat[]) {
-        this.storage.set(STORAGE.CREATES, creates);
-    }
-
-    addMultiStorageCreate(createsList: CreatesChat[]) {
-        this.getStorageCreateList().subscribe((createList) => {
-            if (createList) {
-                const creates: CreatesChat[] = createList;
-                creates.concat(createsList);
-                this.storage.set(STORAGE.CREATES, creates);
-            } else {
-                const creates: CreatesChat[] = [];
-                creates.concat(createsList);
-                this.storage.set(STORAGE.CREATES, creates);
-            }
-        });
-    }
-
-    addOneStorageCreate(create: CreatesChat) {
-        this.getStorageCreateList().subscribe((createList) => {
-            if (createList) {
-                const creates: CreatesChat[] = createList;
-                creates.push(create);
-                this.storage.set(STORAGE.CREATES, creates);
-            } else {
-                const creates: CreatesChat[] = [];
-                creates.push(create);
-                this.storage.set(STORAGE.CREATES, creates);
-            }
-        });
     }
 }
