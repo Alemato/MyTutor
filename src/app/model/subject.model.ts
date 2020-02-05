df9f466c5c0cf19078efebbcba6228d1a8a76047
Stefano Decina <stefano.decina@student.univaq.it>
2020-02-05 09:14:26 +0100

    scrematura subject model

--- src/app/model/subject.model.ts
+++ src/app/model/subject.model.ts
@@ -3,41 +3,4 @@ export class Subject {
     macroSubject: string;
     microSubject: string;
 
-    constructor(obj: any) {
-        if (obj !== undefined) {
-            if (obj.idSubject !== undefined) {
-                this.idSubject = obj.idSubject;
-            } else {
-                this.idSubject = 0;
-            }
-            if (obj.macroSubject !== undefined) {
-                this.macroSubject = obj.macroSubject;
-            } else {
-                this.macroSubject = '';
-            }
-            if (obj.microSubject !== undefined) {
-                this.microSubject = obj.microSubject;
-            } else {
-                this.microSubject = '';
-            }
-        } else {
-            this.idSubject = 0;
-            this.macroSubject = '';
-            this.microSubject = '';
-        }
-    }
-
-    set(obj: any) {
-        if (obj !== undefined) {
-            if (obj.idSubject !== undefined) {
-                this.idSubject = obj.idSubject;
-            }
-            if (obj.macroSubject !== undefined) {
-                this.macroSubject = obj.macroSubject;
-            }
-            if (obj.microSubject !== undefined) {
-                this.microSubject = obj.microSubject;
-            }
-        }
-    }
 }
