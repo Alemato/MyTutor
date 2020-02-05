f5e29145df4d6dfff58b6f21a6b87b6e37e56f97
Stefano Decina <stefano.decina@student.univaq.it>
2020-02-05 09:12:58 +0100

    scrematura chat model

--- src/app/model/chat.model.ts
+++ src/app/model/chat.model.ts
@@ -1,33 +1,9 @@
+import {Student} from './student.model';
+import {Teacher} from './teacher.model';
+
 export class Chat {
     idChat: number;
+    userListser: [Student, Teacher];
     name: string;
 
-    constructor(obj: any) {
-        if (obj !== undefined) {
-            if (obj.idChat !== undefined) {
-                this.idChat = obj.idChat;
-            } else {
-                this.idChat = 0;
-            }
-            if (obj.chatName !== undefined) {
-                this.name = obj.chatName;
-            } else {
-                this.name = '';
-            }
-        } else {
-            this.idChat = 0;
-            this.name = '';
-        }
-    }
-
-    set(obj: any) {
-        if (obj !== undefined) {
-            if (obj.idChat !== undefined) {
-                this.idChat = obj.idChat;
-            }
-            if (obj.chatName !== undefined) {
-                this.name = obj.chatName;
-            }
-        }
-    }
 }
