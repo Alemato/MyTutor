32ed42702c26e2dc69a76b7bd53ddd6126c37259
Stefano Decina <stefano.decina@student.univaq.it>
2020-02-05 09:14:03 +0100

    scrematura planning model

--- src/app/model/planning.model.ts
+++ src/app/model/planning.model.ts
@@ -6,69 +6,7 @@ export class Planning {
     startTime: string;
     endTime: string;
     available: boolean;
+    repeatPlanning: boolean;
     lesson: Lesson;
 
-    constructor(obj: any, lesson: Lesson) {
-        if (obj !== undefined) {
-            if (obj.idPlanning !== undefined) {
-                this.idPlanning = obj.idPlanning;
-            } else {
-                this.idPlanning = 0;
-            }
-            if (obj.date !== undefined) {
-                this.date = obj.date;
-            } else {
-                this.date = new Date();
-            }
-            if (obj.startTime !== undefined) {
-                this.startTime = obj.startTime;
-            } else {
-                this.startTime = '';
-            }
-            if (obj.endTime !== undefined) {
-                this.endTime = obj.endTime;
-            } else {
-                this.endTime = '';
-            }
-            if (obj.available !== undefined) {
-                this.available = obj.available;
-            } else {
-                this.available = true;
-            }
-        } else {
-            this.idPlanning = 0;
-            this.date = new Date();
-            this.startTime = '';
-            this.endTime = '';
-            this.available = true;
-        }
-        if (lesson !== undefined) {
-            this.lesson = lesson;
-        } else {
-            this.lesson = new Lesson(undefined, undefined, undefined);
-        }
-    }
-
-    set(obj: any, lesson: Lesson) {
-        if (obj !== undefined) {
-            if (obj.idPlanning !== undefined) {
-                this.idPlanning = obj.idPlanning;
-            }
-            if (obj.date !== undefined) {
-                this.date = obj.date;
-            }
-            if (obj.startTime !== undefined) {
-                this.startTime = obj.startTime;
-            }
-            if (obj.endTime !== undefined) {
-                this.endTime = obj.endTime;
-            }
-            if (obj.available !== undefined) {
-                this.available = obj.available;
-            }
-        }
-        if (lesson !== undefined) {
-            this.lesson = lesson;
-        }
-    }
 }
