ebc2168deff0e24328b6b15668f747eec03c65c2
Alessandro Mattei <alessandro.mattei1@student.univaq.it>
2020-02-08 11:24:54 +0100

    DEBUG tabs e home

--- src/app/pages/tabs/tabs.page.ts
+++ src/app/pages/tabs/tabs.page.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import {UserService} from '../../services/user.service';
 import {BehaviorSubject} from 'rxjs';
 import {User} from '../../model/user.model';
+import {Router} from '@angular/router';
 
 @Component({
   selector: 'app-tabs',
@@ -10,11 +11,17 @@ import {User} from '../../model/user.model';
 })
 export class TabsPage implements OnInit {
   private user$: BehaviorSubject<User>;
+  isSearch = false;
 
-  constructor(private userService: UserService) { }
+  constructor(private userService: UserService,
+              private route: Router) { }
 
   ngOnInit() {
     this.user$ = this.userService.getUser();
+    console.log(this.route.url);
   }
 
+  isInSearch() {
+    this.isSearch = ('/tabs/ricerca-lezioni' === this.route.url);
+  }
 }
