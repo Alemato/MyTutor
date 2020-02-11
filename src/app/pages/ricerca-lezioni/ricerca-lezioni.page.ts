88dcb8762c2fe2add2b09a0f66558e063dd60a93
Alessandro Mattei <alessandro.mattei1@student.univaq.it>
2020-02-11 16:02:06 +0100

    bozza vuota ricerca lezioni

---
+++ src/app/pages/ricerca-lezioni/ricerca-lezioni.page.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-ricerca-lezioni',
+  templateUrl: './ricerca-lezioni.page.html',
+  styleUrls: ['./ricerca-lezioni.page.scss'],
+})
+export class RicercaLezioniPage implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
