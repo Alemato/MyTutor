37085d4c7b10ab11a81bc9d0e6a4612d29764158
Titi Kevin <titikevin93@gmail.com>
2020-02-12 12:05:45 +0100

    traduzioni sulla Tabs

--- src/app/pages/tabs/tabs.module.ts
+++ src/app/pages/tabs/tabs.module.ts
@@ -7,6 +7,7 @@ import {IonicModule} from '@ionic/angular';
 
 import {TabsPage} from './tabs.page';
 import {AuthGuard} from '../../guards/auth.guard';
+import {TranslateModule} from '@ngx-translate/core';
 
 const routes: Routes = [
     {
@@ -82,7 +83,8 @@ const routes: Routes = [
         CommonModule,
         FormsModule,
         IonicModule,
-        RouterModule.forChild(routes)
+        RouterModule.forChild(routes),
+        TranslateModule
     ],
     declarations: [TabsPage]
 })
