60c7b2d590c7fa49ef0f4a919d662665f8a7bb65
Stefano Decina <stefano.decina@student.univaq.it>
2020-02-16 15:37:03 +0100

    debug

--- src/app/pages/lista-annunci-publicati/lista-annunci-publicati.page.ts
+++ src/app/pages/lista-annunci-publicati/lista-annunci-publicati.page.ts
@@ -1,10 +1,12 @@
 import {Component, OnInit} from '@angular/core';
 import {Observable} from 'rxjs';
-import {LoadingController, NavController} from '@ionic/angular';
+import {LoadingController, ModalController, NavController} from '@ionic/angular';
 import {TranslateService} from '@ngx-translate/core';
 import {Planning} from '../../model/planning.model';
 import {PlanningService} from '../../services/planning.service';
 import {Router} from '@angular/router';
+import {Lesson} from '../../model/lesson.model';
+import {LessonService} from '../../services/lesson.service';
 
 @Component({
     selector: 'app-lista-annunci-publicati',
@@ -13,19 +15,22 @@ import {Router} from '@angular/router';
 })
 export class ListaAnnunciPublicatiPage implements OnInit {
     public plannings$: Observable<Planning[]>;
+    public lessons$: Observable<Lesson[]>;
     private loading;
     private pleaseWaitMessage: string;
 
     constructor(private planningService: PlanningService,
-                public loadingController: LoadingController,
+                private lessonService: LessonService,
+                private loadingController: LoadingController,
                 private translateService: TranslateService,
                 private router: Router,
+                private modalController: ModalController,
                 private navController: NavController) {
     }
 
     ngOnInit() {
         this.initTranslate();
-        this.listaPlanning();
+        this.listaLezioni();
     }
     async loadingPresent() {
         this.loading = await this.loadingController.create({
@@ -45,13 +50,39 @@ export class ListaAnnunciPublicatiPage implements OnInit {
         });
     }
 
-    listaPlanning() {
+    listaLezioni() {
         this.plannings$ = this.planningService.getRestPlanningsAsLesson();
+        this.lessons$ = this.lessonService.getRestLessonsWithoutPlanning();
     }
 
-    modificaLezione(planning: Planning) {
+    pianificazioni(planning: Planning) {
+        const root = this.router.config.find(r => r.path === 'lista-pianificazioni');
+        root.data = {isInsert: false, noPlanning: false, urlLezione: '', idLezione: planning.lesson.idLesson};
+        this.navController.navigateRoot('lista-pianificazioni');
+    }
+    addPianificazioni(lezione: any) {
+        const root = this.router.config.find(r => r.path === 'lista-pianificazioni');
+        if (lezione instanceof Planning) {
+            root.data = {isInsert: false, noPlanning: true, urlLezione: '', idLezione: lezione.lesson.idLesson};
+        } else {
+            root.data = {isInsert: false, noPlanning: true, urlLezione: '', idLezione: lezione.idLesson};
+        }
+        this.navController.navigateRoot('lista-pianificazioni');
+    }
+
+    modificaLezione(lezione: any) {
         const root = this.router.config.find(r => r.path === 'inserimento-lezioni');
-        root.data = {isInsert: false, lesson: planning.lesson};
+        if (lezione instanceof Planning) {
+            root.data = {isInsert: false, lesson: lezione.lesson};
+        } else {
+            root.data = {isInsert: false, lesson: lezione};
+        }
+        this.navController.navigateRoot('inserimento-lezioni');
+    }
+
+    creaLezione() {
+        const root = this.router.config.find(r => r.path === 'inserimento-lezioni');
+        root.data = {isInsert: true, lesson: {}, listaAnnunci: true};
         this.navController.navigateRoot('inserimento-lezioni');
     }
 }
