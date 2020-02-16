31a487abf197e7e296fbbbe2c3e8b1331db7c097
Stefano Decina <stefano.decina@student.univaq.it>
2020-02-16 15:36:40 +0100

    debug

--- src/app/pages/dettagli-pianificazione-modal-page/dettagli-pianificazione-modal-page.page.ts
+++ src/app/pages/dettagli-pianificazione-modal-page/dettagli-pianificazione-modal-page.page.ts
@@ -1,6 +1,6 @@
 import {Component, OnInit} from '@angular/core';
 import {FormBuilder, FormGroup, Validators} from '@angular/forms';
-import {ModalController, NavParams} from '@ionic/angular';
+import {AlertController, ModalController, NavParams} from '@ionic/angular';
 import {Planning} from '../../model/planning.model';
 import {DatePipe} from '@angular/common';
 
@@ -17,10 +17,13 @@ export class DettagliPianificazioneModalPage implements OnInit {
     private hoursStart = [];
     private hoursEnd = [];
     private gionoDellaSettimana = '';
+    private cambioOra = true;
+    private cambioRepeat = true;
 
     constructor(private formBuilder: FormBuilder,
                 private modalController: ModalController,
                 private navParams: NavParams,
+                private alertController: AlertController,
                 public datepipe: DatePipe) {
     }
 
@@ -45,17 +48,23 @@ export class DettagliPianificazioneModalPage implements OnInit {
             this.planningFormModel.controls.startTime.disable();
             this.planningFormModel.controls.endTime.disable();
         }
-        this.oggi = new Date().toISOString();
-        if (this.planning.repeatPlanning === true) {
+        const myDate =  new Date();
+        myDate.setHours(myDate.getHours() + 25);
+        this.oggi = myDate.toISOString();
+        if (this.planning.repeatPlanning && !this.navParams.data.gruppo) {
             this.planningFormModel.controls.repeatPlanning.disable();
         }
     }
 
     alCambioData() {
-        this.planningFormModel.controls.startTime.enable();
-        this.gionoDellaSettimana = this.datepipe.transform(this.planningFormModel.value.date, 'EEEE', 'GMT+1', 'it-IT');
+        this.cambioOra = false;
+        this.planningFormModel.controls.startTime.disable();
+        this.planningFormModel.controls.endTime.disable();
         this.planningFormModel.controls.startTime.reset();
+        this.planningFormModel.controls.startTime.enable();
         this.planningFormModel.controls.endTime.reset();
+        this.cambioOra = true;
+        this.gionoDellaSettimana = this.datepipe.transform(this.planningFormModel.value.date, 'EEEE', 'GMT+1', 'it-IT');
         this.hoursStart = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22];
         const date: number[] = this.navParams.data.date;
         const oreInizioEFine: any[] = this.navParams.data.ore;
@@ -70,43 +79,53 @@ export class DettagliPianificazioneModalPage implements OnInit {
                 }
             });
         }
+        if (this.datepipe.transform(this.oggi, 'yyyy/MM/dd') === this.datepipe.transform(this.planningFormModel.value.date, 'yyyy/MM/dd')) {
+            const indiceDataOggi = this.hoursStart.findIndex(o => o === parseInt((new Date(this.oggi).getHours() + 1).toString(), 0));
+            if (indiceDataOggi !== -1) {
+                this.hoursStart.splice(0, indiceDataOggi);
+            }
+        }
     }
 
     alCambioOraInizio() {
-        this.planningFormModel.controls.endTime.enable();
-        this.planningFormModel.controls.endTime.reset();
-        this.hoursEnd = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23];
-        const date: number[] = this.navParams.data.date;
-        const oreInizioEFine: any[] = this.navParams.data.ore;
-        const indiceData = date.findIndex(
-            d => d === new Date(this.datepipe.transform(this.planningFormModel.value.date, 'yyyy/MM/dd')).getTime());
-        if (indiceData !== -1) {
-            if (this.planningFormModel.value.startTime.length === 8) {
-                this.hoursEnd.splice(0, parseInt(this.planningFormModel.value.startTime.slice(0, 2), 0) + 1);
-            } else {
-                this.hoursEnd.splice(0, parseInt(new Date(this.planningFormModel.value.startTime).toLocaleTimeString().slice(0, 2), 0) + 1);
-            }
-            oreInizioEFine[indiceData].forEach((ora) => {
-                const indiceOra = this.hoursEnd.findIndex(o => o === parseInt(ora.endTime.slice(0, 2), 0));
-                if (indiceOra !== -1) {
-                    if (indiceOra === 0) {
-                        this.hoursEnd.splice(1, 24);
-                    } else {
-                        this.hoursEnd.splice(indiceOra, 24);
-                    }
+        if (this.cambioOra) {
+            this.planningFormModel.controls.endTime.enable();
+            this.planningFormModel.controls.endTime.reset();
+            this.hoursEnd = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23];
+            const date: number[] = this.navParams.data.date;
+            const oreInizioEFine: any[] = this.navParams.data.ore;
+            const indiceData = date.findIndex(
+                d => d === new Date(this.datepipe.transform(this.planningFormModel.value.date, 'yyyy/MM/dd')).getTime());
+            if (indiceData !== -1) {
+                if (this.planningFormModel.value.startTime.length === 8) {
+                    this.hoursEnd.splice(0, parseInt(this.planningFormModel.value.startTime.slice(0, 2), 0) + 1);
+                } else {
+                    // tslint:disable-next-line:max-line-length
+                    this.hoursEnd.splice(0, parseInt(new Date(this.planningFormModel.value.startTime).toLocaleTimeString().slice(0, 2), 0) + 1);
                 }
-            });
-        } else {
-            if (this.planningFormModel.value.startTime.length === 8) {
-                this.hoursEnd.splice(0, parseInt(this.planningFormModel.value.startTime.slice(0, 2), 0) + 1);
+                oreInizioEFine[indiceData].forEach((ora) => {
+                    const indiceOra = this.hoursEnd.findIndex(o => o === parseInt(ora.endTime.slice(0, 2), 0));
+                    if (indiceOra !== -1) {
+                        if (indiceOra === 0) {
+                            this.hoursEnd.splice(1, 24);
+                        } else {
+                            this.hoursEnd.splice(indiceOra, 24);
+                        }
+                    }
+                });
             } else {
-                this.hoursEnd.splice(0, parseInt(new Date(this.planningFormModel.value.startTime).toLocaleTimeString().slice(0, 2), 0) + 1);
+                if (this.planningFormModel.value.startTime.length === 8) {
+                    this.hoursEnd.splice(0, parseInt(this.planningFormModel.value.startTime.slice(0, 2), 0) + 1);
+                } else {
+                    // tslint:disable-next-line:max-line-length
+                    this.hoursEnd.splice(0, parseInt(new Date(this.planningFormModel.value.startTime).toLocaleTimeString().slice(0, 2), 0) + 1);
+                }
             }
         }
     }
 
     gestioneOre() {
-        this.gionoDellaSettimana = this.datepipe.transform(this.planningFormModel.value.date, 'EEEE', 'GMT+1', 'it-IT');
+        this.gionoDellaSettimana = this.datepipe.transform(this.planningFormModel.value.date, 'EEEE', 'GMT+2', 'it-IT');
         this.hoursStart = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22];
         const date: number[] = this.navParams.data.date;
         const oreInizioEFine: any[] = this.navParams.data.ore;
@@ -163,11 +182,49 @@ export class DettagliPianificazioneModalPage implements OnInit {
         }
     }
 
+    async presentaAlert() {
+        if (this.planning.repeatPlanning && !this.planningFormModel.value.repeatPlanning && this.cambioRepeat) {
+            const alert = await this.alertController.create({
+                header: 'Attenzione',
+                message: 'Togliendo la ripetizione settimanale verranno eliminate tutte le pianificazioni programmate ' +
+                    'settimanalmente tranne la prima disponibile, la quale diventerà pianificazione non ripetuta',
+                buttons: [
+                    {
+                        text: 'Annulla',
+                        role: 'cancel',
+                        cssClass: 'secondary',
+                        handler: () => {
+                            this.cambioRepeat = false;
+                            this.planningFormModel.controls.repeatPlanning.setValue(true);
+                        }
+                    }, {
+                        text: 'Ok',
+                        handler: () => {
+                            alert.dismiss();
+                        }
+                    }]
+            });
+
+            await alert.present();
+        }
+    }
+
     async salvaPlanning() {
-        this.planning.date = new Date(new Date(this.datepipe.transform(this.planningFormModel.value.date, 'yyyy/MM/dd')).getTime());
-        this.planning.startTime = new Date(this.planningFormModel.value.startTime).toLocaleTimeString();
-        this.planning.endTime = new Date(this.planningFormModel.value.endTime).toLocaleTimeString();
-        this.planning.repeatPlanning = this.planningFormModel.value.repeatPlanning;
+        this.planning.date = new Date(this.datepipe.transform(this.planningFormModel.value.date, 'yyyy/MM/dd')).getTime();
+        if (this.planningFormModel.value.startTime.length === 8) {
+            this.planning.startTime = this.planningFormModel.value.startTime;
+        } else {
+            this.planning.startTime = new Date(this.planningFormModel.value.startTime).toLocaleTimeString();
+        }
+        if (this.planningFormModel.value.endTime.length === 8) {
+            this.planning.endTime = this.planningFormModel.value.endTime;
+        } else {
+            this.planning.endTime = new Date(this.planningFormModel.value.endTime).toLocaleTimeString();
+        }
+        if (this.planning.repeatPlanning === true) {
+        } else {
+            this.planning.repeatPlanning = this.planningFormModel.value.repeatPlanning;
+        }
         await this.modalController.dismiss(this.planning);
     }
 
