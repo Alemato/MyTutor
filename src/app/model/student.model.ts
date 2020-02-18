8a9eabd245b7a80b436594b90ca51480bb537646
Alessandro Mattei <alessandro.mattei1@student.univaq.it>
2020-02-18 14:07:30 +0100

    set student from FORM REGISTRAZIONE

--- src/app/model/student.model.ts
+++ src/app/model/student.model.ts
@@ -4,4 +4,17 @@ export class Student extends User {
     idStudent: number;
     studyGrade: string;
 
+    setStudentFromUser(user: User) {
+        this.idUser = user.idUser;
+        this.email = user.email;
+        this.password = user.password;
+        this.roles = user.roles;
+        this.name = user.name;
+        this.surname = user.surname;
+        this.birthday = user.birthday;
+        this.language = user.language;
+        this.idStudent  = 0;
+        this.studyGrade = '';
+    }
+
 }
