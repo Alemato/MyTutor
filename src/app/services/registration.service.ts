ea031c50e0b15a815d7aa2fd316656eff6cea405
Alessandro Mattei <alessandro.mattei1@student.univaq.it>
2020-02-19 17:58:33 +0100

    doc registration

--- src/app/services/registration.service.ts
+++ src/app/services/registration.service.ts
@@ -12,10 +12,18 @@ export class RegistrationService {
 
   constructor(private http: HttpClient) { }
 
+  /**
+   * Funzione che invia un oggetto student per essere savalto sul server
+   * @param student da inviare
+   */
   registrationStudent(student: Student): Observable<any> {
     return this.http.post<any>(URL.REGISTRATION_STUDENT, student, {observe: 'response'});
   }
 
+  /**
+   * Funzione che invia un oggetto teacher per essere savalto sul server
+   * @param teacher da inviare
+   */
   registrationTeacher(teacher: Teacher): Observable<any> {
     return this.http.post<any>(URL.REGISTRATION_TEACHER, teacher, {observe: 'response'});
   }
